a8591e51c46910423668819c586925fd27e85224
Pete Wicken <petewicken@gmail.com>
2023-06-18 17:46:59 +0100

    Format strings benchmark

--- benches/strings.rs
+++ benches/strings.rs
@@ -8,21 +8,27 @@ const PCAP: &str = "./benches/data/SkypeIRC.cap";
 
 fn str(c: &mut Criterion) {
     let mut pktstring_group = Criterion::benchmark_group(c, "Dump Strings");
-    pktstring_group.bench_function(BenchmarkId::new("dump_strings", "defaults_skypeIRC_pcap"), |b| {
-        let filepath = Path::new(PCAP);
-        let mut cap = Capture::from_file(filepath).unwrap();
-        b.iter(|| {
-            strings::dump_strings(&mut cap, &7, &mut None, &false, &None);
-        });
-    });
-    pktstring_group.bench_function(BenchmarkId::new("dump_strings", "resolve_skypeIRC_pcap"), |b| {
-        let mut resolver: Option<Box<net::Resolver>> = Some(Box::default());
-        let filepath = Path::new(PCAP);
-        let mut cap = Capture::from_file(filepath).unwrap();
-        b.iter(|| {
-            strings::dump_strings(&mut cap, &7, &mut resolver, &false, &None);
-        });
-    });
+    pktstring_group.bench_function(
+        BenchmarkId::new("dump_strings", "defaults_skypeIRC_pcap"),
+        |b| {
+            let filepath = Path::new(PCAP);
+            let mut cap = Capture::from_file(filepath).unwrap();
+            b.iter(|| {
+                strings::dump_strings(&mut cap, &7, &mut None, &false, &None);
+            });
+        },
+    );
+    pktstring_group.bench_function(
+        BenchmarkId::new("dump_strings", "resolve_skypeIRC_pcap"),
+        |b| {
+            let mut resolver: Option<Box<net::Resolver>> = Some(Box::default());
+            let filepath = Path::new(PCAP);
+            let mut cap = Capture::from_file(filepath).unwrap();
+            b.iter(|| {
+                strings::dump_strings(&mut cap, &7, &mut resolver, &false, &None);
+            });
+        },
+    );
     pktstring_group.finish();
 }
 
